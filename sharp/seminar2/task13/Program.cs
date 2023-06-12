4aa21e54e9981b8a4b0d971ff3b701f3d3f84015
KristinaNikanova <efremovaks@kanon.pro>
2023-06-13 00:11:12 +0300

    ДЗ по семинару 2 C#

---
+++ sharp/seminar2/task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = (int) Math.Log10(num) + 1;
+
+if (count < 3) Console.WriteLine("Количество цифр меньше трёх");
+
+count -= 3;
+num /= (int) Math.Pow(10, count);
+
+Console.WriteLine("Третья цифра: " + num % 10);
