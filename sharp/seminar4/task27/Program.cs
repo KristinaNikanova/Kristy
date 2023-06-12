b6cb235b73098f4e74c75ad8852b80573633364e
KristinaNikanova <efremovaks@kanon.pro>
2023-06-13 00:18:32 +0300

    ДЗ по семинару 4 C#

---
+++ sharp/seminar4/task27/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+while(number != 0)
+{
+    summ = summ + number % 10; 
+    number = number/10;
+}
+Console.WriteLine(summ);
