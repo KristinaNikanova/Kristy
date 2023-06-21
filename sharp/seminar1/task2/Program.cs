73e383141f1c5a491361fe7e8f4c3b60faed3c33
KristinaNikanova <131526731+KristinaNikanova@users.noreply.github.com>
2023-06-21 22:16:46 +0300

    Revert "Seminar6"

--- sharp/seminar1/task2/Program.cs
+++
@@ -1,9 +0,0 @@
-Console.WriteLine("Введите первое число");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int b = Convert.ToInt32(Console.ReadLine());
-
-if (a > b) Console.WriteLine("max = " + a + ", min = " + b);
-else if (b > a) Console.WriteLine("max = " + b + ", min = " + a);
-else Console.WriteLine("числа равны");
-
